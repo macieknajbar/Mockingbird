cf7d1b585d50b877929319df99c2ad0692125a0a
Maciej Najbar <maciek.najbar@gmail.com>
2024-02-15 13:29:50 +0100

    Mockingbird init

---
+++ Sources/Mockingbird/Mock.swift
@@ -0,0 +1,162 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maciej Najbar on 15/02/2024.
+//
+
+import Foundation
+
+public class Mock<T> {
+    private let instance: T
+    
+    let uninitializedValue = NSObject()
+    let uninitializedLambdaCall: (Any...) -> Void = { it in }
+    
+    var isStubbing = false
+    var stubs: Dictionary<String, Any> = [:]
+    var lambdaCalls: Dictionary<String, (Any...) -> Void> = [:]
+    var counterCall: Dictionary<String, Int> = [:]
+    var expectations: Dictionary<String, Expectations> = [:]
+    var lastParams: Dictionary<String, Array<Any>> = [:]
+    
+    public var lastValues: Dictionary<String, Any> = [:]
+    
+    public init(instance anInstance: T) {
+        instance = anInstance
+    }
+    
+    public func on(block: (T) -> Any?) -> OngoingMock<T> {
+        assert(!isStubbing, "Incomplete mock definition!")
+        isStubbing = true
+        
+        let _ = block(instance)
+        
+        return OngoingMock(aMock: self)
+    }
+    
+    public func withStubName<T2>(_ name: String, _ params: Any..., block: (T2?) -> T2) -> T2 {
+        if isStubbing {
+            cleanState()
+            
+            stubs[name] = uninitializedValue
+            counterCall[name] = 0
+            expectations[name] = Expectations(isMocking: true, times: -1, params: params, ignoredParams: [])
+        } else {
+            counterCall[name]! += 1
+            lastParams[name] = params
+            lambdaCalls[name]?(params)
+        }
+        
+        let result = block(stubs[name] as? T2)
+        lastValues[name] = result
+        
+        return block(result)
+    }
+    
+    private func cleanState() {
+        if var expectation = expectations.first(where: { it in it.value.isMocking }) {
+            expectation.value.isMocking = false
+            expectations[expectation.key] = expectation.value
+        }
+        if let entry = stubs.first(where: { it in it.value as? NSObject == uninitializedValue }) {
+            stubs.removeValue(forKey: entry.key)
+        }
+    }
+    
+    public func verify() {
+        for (key, value) in expectations {
+            if value.times < 0 {
+                continue
+            }
+            assert(
+                value.times == counterCall[key],
+                "\nMethod \"\(key)\" invoked \(counterCall[key]!) times, when expected \(value.times)."
+            )
+            if value.times == 0 {
+                continue
+            }
+            
+            for idx in 0 ..< value.params.count {
+                if value.ignoredParams.contains(idx) {
+                    continue
+                }
+                
+                assert(
+                    value.params[idx] as? NSObject == lastParams[key]![idx] as? NSObject,
+                    """
+                    Method \"\(key)\" got different params than expected.
+                    Expected: \(String(describing: value.params))
+                    Actual:   \(String(describing: lastParams[key]))
+                    """
+                )
+            }
+            
+        }
+    }
+    
+    struct Expectations {
+        var isMocking: Bool
+        var times: Int
+        var params: Array<Any>
+        var ignoredParams: Array<Int>
+    }
+}
+
+public class OngoingMock<T> {
+    private let mock: Mock<T>
+    
+    init(aMock: Mock<T>) {
+        mock = aMock
+    }
+    
+    public func willReturn(_ value: Any?) -> OngoingMock {
+        mock.isStubbing = false
+        
+        guard let entry = mock.stubs.first(where: { it in it.value as? NSObject == mock.uninitializedValue }) else {
+            print("You must stub only methods that return \"withStubName\"!")
+            return self
+        }
+        mock.stubs[entry.key] = value
+        
+        return self
+    }
+    
+    public func expectCall(times: Int = 1) -> OngoingMock {
+        mock.isStubbing = false
+        
+        guard var entry = mock.expectations.first(where: {it in it.value.isMocking }) else {
+            assert(false, "You must mock only methods that return \"withStubName\"!")
+            return self
+        }
+        entry.value.times = times
+        mock.expectations[entry.key] = entry.value
+        
+        return self
+    }
+    
+    public func ignoringParams(at: Int...) -> OngoingMock {
+        mock.isStubbing = false
+        
+        guard var entry = mock.expectations.first(where: {it in it.value.isMocking }) else {
+            assert(false, "You must mock only methods that return \"withStubName\"!")
+            return self
+        }
+        entry.value.ignoredParams = at
+        mock.expectations[entry.key] = entry.value
+        
+        return self
+    }
+    
+    public func call(_ block: @escaping (Any...) -> Void) -> OngoingMock {
+        mock.isStubbing = false
+        
+        guard let (key, _) = mock.expectations.first(where: {it in it.value.isMocking }) else {
+            assert(false, "You must mock only methods that return \"withStubName\"!")
+            return self
+        }
+        mock.lambdaCalls[key] = block
+        
+        return self
+    }
+}
--- Tests/MockingbirdTests/MockingbirdTests.swift
+++ Tests/MockingbirdTests/MockingbirdTests.swift
@@ -10,3 +10,199 @@ final class MockingbirdTests: XCTestCase {
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
 }
+
+//import UIKit
+//
+//func testTimestampProvider() {
+//    let expected = 44.0
+//    let timestampProvider = TimestampProviderMock { mock in
+//        mock.on { $0.get() }.willReturn(44.0)
+//        return
+//    }
+//    
+//    let actual = timestampProvider.get()
+//    
+//    assert(
+//        expected == actual,
+//        "Assertion failed:\nExpected: \(expected)\nActual: \(actual)"
+//    )
+//}
+//
+//testTimestampProvider()
+//
+//func testPrintDocument() {
+//    let document = "Very important document"
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: document) }.expectCall()
+//        return
+//    }
+//    
+//    Company(printer: printer)
+//        .printDocument(document: document)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintDocument()
+//
+//func testPrintDocument2() {
+//    let document = "Very important document"
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: "") }.ignoringParams(at: 0).expectCall()
+//        return
+//    }
+//    
+//    Company(printer: printer)
+//        .printDocument(document: document)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintDocument2()
+//
+//func testPrintDocumentWithRealTimestamp() {
+//    let document = "Very important document"
+//    let timestampProvider = RealTimestampProvider()
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: document, timestamp: 44)}.ignoringParams(at: 1).expectCall()
+//        return
+//    }
+//    
+//    Company(
+//        printer: printer,
+//        timestampProvider: timestampProvider
+//    ).printDocument(document: document)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintDocumentWithRealTimestamp()
+//
+//func testPrintAnyDocumentWithRealTimestamp() {
+//    let document = "Very important document"
+//    let timestampProvider = RealTimestampProvider()
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: "", timestamp: 44)}.ignoringParams(at: 0, 1).expectCall()
+//        return
+//    }
+//    
+//    Company(
+//        printer: printer,
+//        timestampProvider: timestampProvider
+//    ).printDocument(document: document)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintAnyDocumentWithRealTimestamp()
+//
+//func testPrintDocumentWithTimestamp() {
+//    let document = "Very important document"
+//    let timestampProvider = TimestampProviderMock { mock in
+//        mock.on { $0.get() }.willReturn(44.0)
+//        return
+//    }
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: document, timestamp: 44)}.expectCall()
+//        return
+//    }
+//    
+//    Company(
+//        printer: printer,
+//        timestampProvider: timestampProvider
+//    ).printDocument(document: document)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintDocumentWithTimestamp()
+//
+//func testPrintDocumentWithSignature() {
+//    let document = "Very important document"
+//    let employee = Employee(name: "Maciej Najbar")
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: document, signature: SignatureMock()) }.expectCall()
+//        return
+//    }
+//    
+//    Company(printer: printer)
+//        .printDocument(document: document, employee: employee)
+//    
+//    printer.mock.verify()
+//}
+//
+//testPrintDocumentWithSignature()
+//
+//func testRepository() {
+//    let id = "id"
+//    let expected = "expected value"
+//    let repository = RepositoryMock { mock in
+//        mock.on { $0.save(id: id, record: expected) }.expectCall()
+//        mock.on { $0.read(id: id)! }.willReturn(expected).expectCall()
+//    }
+//    
+//    repository.save(id: id, record: expected)
+//    let actual = repository.read(id: id)
+//    
+//    assert(expected == actual)
+//    repository.mock.verify()
+//}
+//
+//testRepository()
+//
+//func testSuccess() {
+//    let expected = "Important response"
+//    let getItem = GetItemMock { mock in
+//        mock.on { $0.getItem(onError: {}, onSuccess: { response in }) }
+//            .call { params in (params[1] as! (String) -> Void)(expected) }
+//        return
+//    }
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: expected )}.expectCall()
+//        return
+//    }
+//    
+//    let sut: (GetItem, Printer) -> Void = { getItem, printer in
+//        getItem.getItem(onError: {}) { printer.print(document: $0) }
+//    }
+//    sut(getItem, printer)
+//    
+//    printer.mock.verify()
+//}
+//
+//testSuccess()
+//
+//func testFailure() {
+//    let expected = "Important response"
+//    let getItem = GetItemMock { mock in
+//        mock.on { $0.getItem(onError: {}, onSuccess: { response in }) }
+//            .call { params in (params[0] as! () -> Void)() }
+//        return
+//    }
+//    let printer = PrinterMock { mock in
+//        mock.on { $0.print(document: expected )}.expectCall(times: 0)
+//        return
+//    }
+//    
+//    let sut: (GetItem, Printer) -> Void = { getItem, printer in
+//        getItem.getItem(onError: {}) { printer.print(document: $0) }
+//    }
+//    sut(getItem, printer)
+//    
+//    printer.mock.verify()
+//}
+//
+//testFailure()
+//
+//func testReturnNil() {
+//    let timestampProvider = TimestampProviderMock { mock in
+//        mock.on { $0.get() }.willReturn(nil).expectCall()
+//        return
+//    }
+//    
+//    timestampProvider.get()
+//    
+//    timestampProvider.mock.verify()
+//}
+//
+//testReturnNil()
